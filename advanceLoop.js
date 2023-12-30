1a3905f7aae2b78f84bb0caaea49c10e2d6a8466
unknown <hrsafin2434@gmail.com>
2023-12-30 15:27:12 +0600

    forEach array method

---
+++ advanceLoop.js
@@ -0,0 +1,17 @@
+// forEach > It's important to note that forEach is not suitable for all situations.
+            // If you need to transform the elements of an array and create a new array , 
+            // with the results you might want to use map. If you need to filter elements based on a condition,
+            // you can use filter. Each array iteration method serves a different purpose, 
+            // so choose the one that best fits your needs.
+
+
+const newArray = [1,2,3,4,5,6,7]
+
+newArray.forEach((elem) => {
+    console.log(elem, "safin")
+})
+
+
+
+
+
